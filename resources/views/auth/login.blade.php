436f37c9440ffdc7a1d6d6f7047976747c67ed48
{reiseipasuta} <{resonate1794.0714@gmail.com}>
2023-11-02 15:50:06 +0900

    レイアウト修正

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -10,14 +10,14 @@
             @csrf
 
             <!-- Email Address -->
-            <div>
+            <div class="bottomMar10">
                 <x-input-label for="email" :value="__('メールアドレス')" />
                 <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
                 <x-input-error :messages="$errors->get('email')" class="mt-2" />
             </div>
 
             <!-- Password -->
-            <div class="mt-4">
+            <div class="mt-4 bottomMar10">
                 <x-input-label for="password" :value="__('パスワード')" />
 
                 <x-text-input id="password" class="block mt-1 w-full"
@@ -36,7 +36,7 @@
                 </label>
             </div>
 
-            <div class="flex items-center justify-end mt-4">
+            <div class="flex items-center justify-end mt-4 bottomMar10">
                 @if (Route::has('password.request'))
                     <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
                         {{ __('パスワードをお忘れの方') }}
