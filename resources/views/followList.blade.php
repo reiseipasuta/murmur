6ab0b1eddd82d2906aaa6358b7410a46996b627b
{reiseipasuta} <{resonate1794.0714@gmail.com}>
2023-11-08 23:06:05 +0900

    フォローリスト修正

--- resources/views/followList.blade.php
+++ resources/views/followList.blade.php
@@ -13,7 +13,35 @@
             </p>
         @else
             @foreach ($follows as $follow)
-                <div class="post">
+            <div class="post">
+            <div class="profileIcon">
+                <figure class="iconCircle">
+                    @if ($follow->image === null)
+                    <img class="iconImage" src="{{ asset('storage/images/default.png') }}" alt="">
+                    @else
+                    <img class="iconImage" src="{{ asset('storage/images/'.$follow->image) }}" alt="">
+                    @endif
+                </figure>
+                <p class="name">{{ $follow->name }}</p>
+                <div class="rightLink followListFlex">
+                    @if ($title == 'フォロー')
+                    <form action="{{ route('followDestroy', $follow) }}" method="POST">
+                        @csrf
+                        @method('DELETE')
+                        <button class="followingListBtn">フォロー解除</button>
+                    </form>
+                    @endif
+                    {{-- <div class="followListProf">
+                        {{ Str::limit($follow->profile, 150, '...') }}
+                    </div> --}}
+                </div>
+            </div>
+            <div class="followListProf">
+                {{ Str::limit($follow->profile, 150, '...') }}
+            </div>
+            </div>
+
+                {{-- <div class="post">
                     <div class="profileIcon">
                     <figure class="iconCircleSmall">
                         <a href="{{ route('userPage', $follow) }}">
@@ -29,8 +57,16 @@
                             {{ $follow->name }}
                         </a>
                     </p>
+                    <div class="">
+                        <div>
+                            フォロー解除
+                        </div>
+                        <p>
+                            {{ Str::limit($follow->profile, 40, '...') }}
+                        </p>
                     </div>
-                </div>
+                    </div>
+                </div> --}}
             @endforeach
         @endif
     </div>
